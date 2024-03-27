09228c3e935792684d8fd0d017bc3dce699e021c
rinklepoonia <rinklepoonia889@gmail.com>
2024-03-27 11:59:27 +0530

    slider-done

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,33 @@ menuIcon.addEventListener("click", function () {
     document.body.classList.toggle("overFlowHidden")
     document.querySelector(".menuIcon").classList.toggle("cross")
 })
+
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: ".next",
+    prevArrow: ".prev",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+            }
+        }
+    ]
+});
