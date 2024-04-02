c024a4655100bf9fdcf3e15a5a7d91308f4a7647
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-02 09:27:02 +0530

    back-to-top

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,20 @@ menuIcon.addEventListener("click", function () {
     document.body.classList.toggle("overFlowHidden")
     document.querySelector(".menuIcon").classList.toggle("cross")
 })
+// back-to-top
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+});
 
 $('.responsive').slick({
     dots: false,
