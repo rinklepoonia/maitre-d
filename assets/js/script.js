c98ddc89148ec81704f0820929a65db30d41922c
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-03 16:07:20 +0530

    search-icon, hero-bg , feaute-section box layer , feature-section bg-img etc.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ const topBtn = document.querySelector(".topBtn");
 const show = document.querySelector(".show");
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 60) {
+    if (window.scrollY > 300) {
         topBtn.classList.add("show");
     } else {
         topBtn.classList.remove("show");
